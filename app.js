55fccfd70396dff3f6a1eab529f0f1e688ce1e74
Futurescaper23 <phil@futurescaping.co.uk>
2026-08-18 16:22:27 +0100

    Fix Area 2 trend panel image URL

--- app.js
+++ app.js
@@ -5497,7 +5497,7 @@ function trendImageSrcForArea(areaId, fallbackSrc) {
   if (areaId !== "area2") {
     return fallbackSrc;
   }
-  return "/public/projects/padstow-estuary/assets/stats/area2-trend-classification-panel-trebetherick.svg";
+  return "/projects/padstow-estuary/assets/stats/area2-trend-classification-panel-trebetherick.svg";
 }
 
 function trendMapCaptionForArea(areaId, roundCount) {
